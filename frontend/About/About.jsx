2ee0209f4499e55512263d76a7da4250f18bc31d
Reni98 <93825160+Reni98@users.noreply.github.com>
2026-05-06 11:37:18 +0200

    Add files via upload

---
+++ frontend/About/About.jsx
@@ -0,0 +1,25 @@
+import './About.css'
+const About = () => {
+    
+    return(
+         <div className="about-contant">
+        <h1>About</h1>
+        <div className="about-elements">
+            <section className='about-text'>
+                <div className="about-picture">
+                    <div className="about-text-content">
+                        <h2>About Text</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus iusto, assumenda rerum, rem similique neque sed deleniti veniam repudiandae, laborum tempora ut eum! Alias nulla quaerat ipsum magnam asperiores enim!
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+    </div>
+    )
+   
+    
+}
+
+export default About
